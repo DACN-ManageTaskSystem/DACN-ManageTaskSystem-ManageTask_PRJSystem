00154796df8f21aff280b18d651d73cf1161f246
Yesimvu <phamnguyenvu287@gmail.com>
2024-11-28 01:10:24 +0700

    Xóa test

---
+++ ManageTaskWeb/ManageTaskWeb/Content/css/trangchu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManageTaskWeb.Content.css
+{
+    public class trangchu : Controller
+    {
+        //
+        // GET: /trangchu/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
